576584ad69f04d31801487bc1019f895c56df07f
Yujin899 <emad76065@gmail.com>
2026-01-06 20:57:47 +0200

    updated

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -10,18 +10,13 @@ const transitionVariants = {
     item: {
         hidden: {
             opacity: 0,
-            filter: 'blur(12px)',
-            y: 12,
+            filter: 'blur(4px)',
+            y: 20,
         },
         visible: {
             opacity: 1,
             filter: 'blur(0px)',
             y: 0,
-            transition: {
-                type: 'spring',
-                bounce: 0.3,
-                duration: 1.5,
-            },
         },
     },
 }
